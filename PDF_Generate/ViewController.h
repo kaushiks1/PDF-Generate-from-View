d82c814415d355ddc7d24c4cb9b2ca63c393b1b0
Leo <Leo@Leos-Macbook-Pro.local>
2017-05-09 17:15:29 +0530

    Update

---
+++ PDF_Generate/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  PDF_Generate
+//
+//  Created by Apple on 03/05/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    
+    
+}
+
+- (IBAction)btnGeneratePDFPress:(id)sender;
+
+@end
+
---
+++ PDF_Generate/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  PDF_Generate
+//
+//  Created by Apple on 03/05/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)btnGeneratePDFPress:(id)sender
+{
+    
+    [self createPDFfromUIView:self.view saveToDocumentsWithFileName:@"NewPDF.pdf"];
+    
+}
+
+
+-(void)createPDFfromUIView:(UIView*)aView saveToDocumentsWithFileName:(NSString*)aFilename
+{
+    // Creates a mutable data object for updating with binary data, like a byte array
+    NSMutableData *pdfData = [NSMutableData data];
+    
+    // Points the pdf converter to the mutable data object and to the UIView to be converted
+    UIGraphicsBeginPDFContextToData(pdfData, aView.bounds, nil);
+    UIGraphicsBeginPDFPage();
+    CGContextRef pdfContext = UIGraphicsGetCurrentContext();
+    
+    
+    // draws rect to the view and thus this is captured by UIGraphicsBeginPDFContextToData
+    
+    [aView.layer renderInContext:pdfContext];
+    
+    // remove PDF rendering context
+    UIGraphicsEndPDFContext();
+    
+    // Retrieves the document directories from the iOS device
+    NSArray* documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES);
+    
+    NSString* documentDirectory = [documentDirectories objectAtIndex:0];
+    NSString* documentDirectoryFilename = [documentDirectory stringByAppendingPathComponent:aFilename];
+    
+    // instructs the mutable data object to write its context to a file on disk
+    [pdfData writeToFile:documentDirectoryFilename atomically:YES];
+    NSLog(@"documentDirectoryFileName: %@",documentDirectoryFilename);
+}
+
+
+
+@end
